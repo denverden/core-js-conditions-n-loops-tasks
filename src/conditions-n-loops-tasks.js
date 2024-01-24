9837ac01e71abdbf8346fc708f25df0997ce412f
DenverDen <denverden@mail.ru>
2024-01-24 11:07:44 +0300

    feat: add solution getMaxNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -38,8 +38,18 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let result;
+
+  if (a > b && a > c) {
+    result = a;
+  } else if (b > c) {
+    result = b;
+  } else {
+    result = c;
+  }
+
+  return result;
 }
 
 /**
